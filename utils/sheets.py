b43e9b3f1dab304ef053d3e25a2dada390b02cf8
Felipe Froes <froesfelipe03@gmail.com>
2026-06-01 16:42:16 -0300

    Primeira versão

---
+++ utils/sheets.py
@@ -0,0 +1,39 @@
+# utils/sheets.py
+
+import streamlit as st
+import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+
+@st.cache_resource
+def conectar():
+
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=SCOPES
+    )
+
+    return gspread.authorize(creds)
+
+
+@st.cache_data(ttl=300)
+def carregar_dados():
+
+    gc = conectar()
+
+    planilha = gc.open_by_key(
+        "1JU-8v_mxydxgDFwWg_aSURHBeM0PyOQPkUBe-LqitXk"
+    )
+
+    aba = planilha.worksheet("Pedidos")
+
+    dados = aba.get_all_records()
+
+    return pd.DataFrame(dados)
